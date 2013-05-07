4703f70b280966892e9fa2fa09351032328dd7eb
David Rees <d.w.rees.03@gmail.com>
2013-05-07 23:14:38 +0100

    Put comment inside PHP

--- sql.example.php
+++
@@ -1,6 +0,0 @@
-//Details for a MySQL db connection which can be used for all connections
-
-<?
-mysql_connect("localhost", "dbname", "dbpass") or die(mysql_error());
-mysql_select_db("dbuser") or die(mysql_error());
-?>
