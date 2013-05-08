c540562885f9b4b8351bda99af7db5b02a12925f
David Rees <d.w.rees.03@gmail.com>
2013-05-08 15:52:17 +0100

    delete process-sortable.php

--- process-sortable.php
+++
@@ -1,5 +0,0 @@
-include ('sql.php');
-foreach ($_GET['listItem'] as $position => $item) :
-    $position=$position+1;
-    mysql_query("UPDATE `Lesson` SET `lessonno` = $position WHERE `id` = $item");
-endforeach;
